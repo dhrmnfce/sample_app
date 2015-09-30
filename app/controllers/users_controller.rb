63187dfa1f7e9b1ec0f1689544e3dc867603c474
dhrmnfce <dylanludwick@gmail.com>
2015-09-30 17:54:44 +0000

    Modified password reset test for expired password reset, modified users index test to limit users listed via pagination to 10, modified users signup to limit only activated users to be listed on index and available for show, modified user model to update columns instead of attributes for activate and create reset digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,12 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
   
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.where(activated: true).paginate(page: params[:page], per_page: 10)
   end
   
   def show
     @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
   
   def new
